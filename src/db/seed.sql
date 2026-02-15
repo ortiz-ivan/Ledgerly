f2d0510ab0b376335e4d43205c7649021d8dbe5c
Ivan Ortiz <ivanzitro18@gmail.com>
2026-02-14 23:08:40 -0300

    Separacion de graficos del index.html

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -17,21 +17,3 @@ INSERT OR IGNORE INTO categorias (nombre, descripcion) VALUES
 ('Compras', 'Ropa, electrónica, artículos'),
 ('Deudas', 'Pagos de deudas/créditos'),
 ('Otro Egreso', 'Otros gastos varios');
-
--- INSERTAR TRANSACCIONES DE PRUEBA (usando fechas dinámicas)
-INSERT OR IGNORE INTO transacciones (tipo, monto, descripcion, fecha, categoria_id) VALUES
--- Transacciones del mes actual
-('INGRESO', 2500000, 'Salario mensual', DATE('now', 'start of month'), 1),
-('INGRESO', 150000, 'Bonus extra', DATE('now', 'start of month', '+9 days'), 2),
-('EGRESO', 250000, 'Supermercado', DATE('now', 'start of month', '+2 days'), 7),
-('EGRESO', 80000, 'Gasolina', DATE('now', 'start of month', '+4 days'), 8),
-('EGRESO', 450000, 'Alquiler', DATE('now', 'start of month', '+6 days'), 9),
-('EGRESO', 120000, 'Cine y entretenimiento', DATE('now', 'start of month', '+11 days'), 11),
-('EGRESO', 45000, 'Farmacia', DATE('now', 'start of month', '+14 days'), 10),
-
--- Transacciones del mes anterior
-('INGRESO', 2500000, 'Salario mensual', DATE('now', 'start of month', '-1 month'), 1),
-('EGRESO', 280000, 'Compras electrodomésticos', DATE('now', 'start of month', '-1 month', '+9 days'), 12),
-('EGRESO', 450000, 'Alquiler', DATE('now', 'start of month', '-1 month', '+6 days'), 9),
-('EGRESO', 200000, 'Groceries', DATE('now', 'start of month', '-1 month', '+14 days'), 7);
-
